3d55de350f7e485fa5dbbca40af631191362af93
kostin <kostin@payhd.ru>
2025-02-18 10:30:13 +0300

    Рефакторинг

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,30 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/test-pwa/",
-  build: {
-    sourcemap: true,
-    assetsDir: "code",
-    target: ["esnext"],
-    cssMinify: true,
-    lib: false
-  },
-  plugins: [
-    VitePWA({
-      strategies: "injectManifest",
-      injectManifest: {
-        swSrc: 'public/sw.js',
-        swDest: 'dist/sw.js',
-        globDirectory: 'dist',
-        globPatterns: [
-          '**/*.{html,js,css,json,png,svg}',
-        ],
-      },
-      injectRegister: false,
-      manifest: false,
-      devOptions: {
-        enabled: true
-      }
-    })
-  ]
+    base: "/test-pwa/",
+    build: {
+        sourcemap: true,
+        assetsDir: "code",
+        target: ["esnext"],
+        cssMinify: true,
+        lib: false
+    },
+    plugins: [
+        VitePWA({
+            strategies: "injectManifest",
+            injectManifest: {
+                swSrc: 'public/sw.js',
+                swDest: 'dist/sw.js',
+                globDirectory: 'dist',
+                globPatterns: [
+                    '**/*.{html,js,css,json,png,svg}',
+                ],
+            },
+            injectRegister: false,
+            manifest: false,
+            devOptions: {
+                enabled: true
+            }
+        })
+    ]
 })
